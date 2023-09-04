beb65ac9bfa673038746157964b3056271d4e5df
JosefineM22R <josefine.monrad@gmail.com>
2023-09-04 14:51:04 +0200

    Laver box af prikker
    Boxen flyver mod dig oppe fra hjørnet

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
-prik1Pos = [100, 200, -200];
-prik2Pos = [300, 200, -200];
-camPos = [200, 200, 0];
+prik1Pos = [10, 10, -200];
+prik2Pos = [10, -10, -200];
+prik3Pos = [-10, 10, -200];
+prik4Pos = [-10, -10, -200];
+prik5Pos = [10, 10, -250];
+prik6Pos = [10, -10, -250];
+prik7Pos = [-10, 10, -250];
+prik8Pos = [-10, -10, -250];
+camPos = [0, 0, 0];
 screenZ = -100;
 
 prik1Screen = [0, 0];
-prik2Screen = [0, 0]
+prik2Screen = [0, 0];
 
 function setup() 
 {
@@ -14,16 +20,34 @@ function setup()
 function draw() 
 {
   background(220);
+
+  //Punkterne bevæger sig
   prik2Pos[2] += 1;
   prik1Pos[2] += 1;
+  prik3Pos[2] += 1;
+  prik4Pos[2] += 1;
+  prik5Pos[2] += 1;
+  prik6Pos[2] += 1;
+  prik7Pos[2] += 1;
+  prik8Pos[2] += 1;
 
+  //Laver punkter
   makePoint(prik1Pos[0], prik1Pos[1], prik1Pos[2]);
   makePoint(prik2Pos[0], prik2Pos[1], prik2Pos[2]);
+  makePoint(prik3Pos[0], prik3Pos[1], prik3Pos[2]);
+  makePoint(prik4Pos[0], prik4Pos[1], prik4Pos[2]);
+  makePoint(prik5Pos[0], prik5Pos[1], prik5Pos[2]);
+  makePoint(prik6Pos[0], prik6Pos[1], prik6Pos[2]);
+  makePoint(prik7Pos[0], prik7Pos[1], prik7Pos[2]);
+  makePoint(prik8Pos[0], prik8Pos[1], prik8Pos[2]);
 }
 
+//Funktion der laver punkter
 function makePoint(x, y, z)
 {
   let pointScreenX = x * screenZ / z;
   let pointScreenY = y * screenZ / z;
   point(pointScreenX, pointScreenY);
 }
+
+
